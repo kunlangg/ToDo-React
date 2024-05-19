8403292145dbc608bb41d3c8f671f0b3bfab1421
kunlangg <kunlangr@gmail.com>
2024-05-19 23:54:53 +0700

    ToDo

---
+++ src/componens/ToDoList.jsx
@@ -0,0 +1,31 @@
+import { Checkbox, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import React from 'react';
+
+const ToDoList = ({ todos, handleCheckbox }) => {
+    return (
+        <div className="todo-list">
+            <List>
+                {todos.map((todo) => {
+                    return (
+                        <ListItem key={todo.id} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Checkbox
+                                        onChange={() => handleCheckbox(todo.id)}
+                                        disableRipple
+                                    />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary={todo.task}
+                                    className={todo.complete && "completed"}
+                                />
+                            </ListItemButton>
+                        </ListItem>
+                    );
+                })}
+            </List>
+        </div>
+    );
+};
+
+export default ToDoList;
